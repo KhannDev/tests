978f04667d318fa0ec9c498ebe3840aabdec3f36
Muaaz Khan <muaaz@onpar.in>
2025-02-01 18:22:58 +0300

    first commit

--- pages/api/download.js
+++ pages/api/download.js
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs";
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "public", "1424123.pdf"); // Update with your actual file name
+  const filePath = path.join(process.cwd(), "public", "164932.pdf"); // Update with your actual file name
 
   if (!fs.existsSync(filePath)) {
     return res.status(404).json({ error: "File not found" });
@@ -10,7 +10,7 @@ export default function handler(req, res) {
 
   const fileStream = fs.createReadStream(filePath);
 
-  res.setHeader("Content-Disposition", 'attachment; filename="1424123.pdf"');
+  res.setHeader("Content-Disposition", 'attachment; filename="164932.pdf"');
   res.setHeader("Content-Type", "application/pdf");
 
   fileStream.pipe(res);
